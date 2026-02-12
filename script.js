96e48d7462ad5b66c34ecd321085bc041d01dca5
tuanabozdag <tuanabozdag5@gmail.com>
2026-02-12 18:34:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+let topZ = 10;
+
 function closeWin(id) {
   document.getElementById(id).style.display = "none";
 }
 
-function openWin(id) {
-  document.getElementById(id).style.display = "block";
-}
+document.querySelectorAll('.window').forEach(win => {
+  const bar = win.querySelector('.title-bar');
+
+  bar.addEventListener('mousedown', e => {
+    win.style.zIndex = ++topZ;
+
+    let offsetX = e.clientX - win.offsetLeft;
+    let offsetY = e.clientY - win.offsetTop;
+
+    function move(e) {
+      win.style.left = e.clientX - offsetX + "px";
+      win.style.top = e.clientY - offsetY + "px";
+    }
+
+    document.addEventListener('mousemove', move);
+    document.addEventListener('mouseup', () => {
+      document.removeEventListener('mousemove', move);
+    }, { once: true });
+  });
+
+  win.addEventListener('mousedown', () => {
+    win.style.zIndex = ++topZ;
+  });
+});
