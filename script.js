d79f84a080b7a1fead77218a32b46e56477bae5c
tuanabozdag <tuanabozdag5@gmail.com>
2026-02-22 17:23:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,34 @@
-let topZ = 10;
+const openSound = document.getElementById("openSound");
 const clickSound = document.getElementById("clickSound");
 
-function playClick() {
-  if (clickSound) {
-    clickSound.currentTime = 0;
-    clickSound.play();
-  }
-}
+window.onload = () => {
+  openSound.play();
+};
 
 function openWin(id) {
-  playClick();
-  const w = document.getElementById(id);
-  w.style.display = "block";
-  w.style.zIndex = ++topZ;
+  const win = document.getElementById(id);
+  win.style.display = "block";
+  clickSound.play();
 }
 
-function closeWin(id) {
-  playClick();
-  document.getElementById(id).style.display = "none";
-}
+document.querySelectorAll(".window").forEach(win => {
+  const bar = win.querySelector(".titlebar");
+  const closeBtn = win.querySelector(".close");
+  let isDrag = false, x, y;
 
-function toggleStart() {
-  playClick();
-  const m = document.getElementById("startMenu");
-  m.style.display = m.style.display === "block" ? "none" : "block";
-}
-
-/* DEFAULT POZİSYONLAR */
-document.querySelectorAll('.window').forEach(win => {
-  win.style.left = win.dataset.x + "px";
-  win.style.top = win.dataset.y + "px";
-});
-
-/* DRAG */
-document.querySelectorAll('.window').forEach(win => {
-  const bar = win.querySelector('.title-bar');
-
-  bar.addEventListener('mousedown', e => {
-    playClick();
-    win.style.zIndex = ++topZ;
-
-    let ox = e.clientX - win.offsetLeft;
-    let oy = e.clientY - win.offsetTop;
-
-    function move(e) {
-      win.style.left = e.clientX - ox + "px";
-      win.style.top = e.clientY - oy + "px";
-    }
-
-    document.addEventListener('mousemove', move);
-    document.addEventListener('mouseup', () => {
-      document.removeEventListener('mousemove', move);
-    }, { once: true });
+  bar.addEventListener("mousedown", e => {
+    isDrag = true;
+    x = e.clientX - win.offsetLeft;
+    y = e.clientY - win.offsetTop;
   });
-});
 
-/* AUTO FOCUS */
-window.onload = () => {
-  openWin('byf');
-};
+  document.addEventListener("mousemove", e => {
+    if (!isDrag) return;
+    win.style.left = e.clientX - x + "px";
+    win.style.top = e.clientY - y + "px";
+  });
+
+  document.addEventListener("mouseup", () => isDrag = false);
+
+  closeBtn.onclick = () => win.style.display = "none";
+});
